062cc6340aa8d76f5a15cd87e455ce6feb9dbb3f
pssofluiz <luiz.azevedo@topaziopartner.com.br>
2020-03-12 19:26:46 -0300

    add return data user

--- server.js
+++ server.js
@@ -16,47 +16,48 @@ const SECRET_KEY = '123456789'
 const expiresIn = '1h'
 
 // Create a token from a payload 
-function createToken(payload){
-  return jwt.sign(payload, SECRET_KEY, {expiresIn})
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn })
 }
 
 // Verify the token 
-function verifyToken(token){
-  return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err)
 }
 
 // Check if the user exists in database
-function isAuthenticated({email, password}){
+function isAuthenticated({ email, password }) {
   return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
 }
 
 
 server.post('/auth/login', (req, res) => {
-  const {email, password} = req.body
-  if (isAuthenticated({email, password}) === false) {
+  const { email, password } = req.body
+  if (isAuthenticated({ email, password }) === false) {
     const status = 401
     const message = 'Incorrect email or password'
-    res.status(status).json({status, message})
+    res.status(status).json({ status, message })
     return
   }
-  const access_token = createToken({email, password})
-  res.status(200).json({access_token})
+  const access_token = createToken({ email, password })
+  const user = userdb.users.filter(user => user.email === email && user.password === password);
+  res.status(200).json({ access_token, user })
 })
 
-server.use(/^(?!\/auth).*$/,  (req, res, next) => {
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
     const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({status, message})
+    const message = 'Error in authorization format - XX'
+    res.status(status).json({ status, message })
     return
   }
   try {
-     verifyToken(req.headers.authorization.split(' ')[1])
-     next()
+    verifyToken(req.headers.authorization.split(' ')[1])
+    next()
   } catch (err) {
     const status = 401
     const message = 'Error access_token is revoked'
-    res.status(status).json({status, message})
+    res.status(status).json({ status, message })
   }
 })
 
